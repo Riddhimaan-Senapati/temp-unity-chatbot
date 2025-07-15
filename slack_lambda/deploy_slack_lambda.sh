f8bc537d22037ffaffab68b1c8ae7db0b72543d1
Riddhimaan-Senapati <rsenapati@umass.edu>
2025-07-15 17:49:12 -0400

    updated the slack_bot code to work locally as well

--- slack_lambda/deploy_slack_lambda.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-pip install git+https://github.com/nficano/python-lambda
-lambda deploy \
-  --config-file slack_lambda/slack_lambda_config.yaml \
-  --requirements slack_lambda/slack_lambda_requirements.txt
